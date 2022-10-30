86ab3232076eb1bb6a9596ffbbf7c053e6cd0872
ZhouSusan <szhou089@gmail.com>
2022-10-30 04:09:14 -0700

    added sum(n) that sums up all the multiples of threes and fives

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = sum(15);
+        System.out.println(a);
+    }
+
+    public static Integer sum(Integer n) {
+        Integer tot = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                tot += i;
+            }
+        }
+        return tot;
     }
 }
