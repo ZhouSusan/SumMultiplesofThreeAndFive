acceed0f84a30e786bf12da8efeca262aa87bb4c
ZhouSusan <szhou089@gmail.com>
2022-10-30 04:49:02 -0700

    added two more test cases to Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         int a = sum(15);
         System.out.println(a);
+
+        int b = sum(10);
+        System.out.println(b);
+
+        int c = sum(110);
+        System.out.println(c);
     }
 
     public static Integer sum(Integer n) {
